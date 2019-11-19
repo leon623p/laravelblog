46f9b48991755362ab982712264111063647d842
Leon Light <leon@Rebeccasmacbook.ss.shawcable.net>
2019-11-19 10:19:06 -0600

    pushing yesterdays work

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -9,6 +9,12 @@ class Post extends Model
     public function category(){
         return $this->belongsTo('App\Category');
     }
+    public function tags()
+{
+    return $this->belongsToMany('App\Tag');
 }
+}
+
+
 
 
